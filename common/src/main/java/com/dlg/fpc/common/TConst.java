f49ac7c4f0bf08a98473d40cc37f479e7be9f048
lingui <linguiduan@gmail.com>
2025-12-18 17:50:05 +0800

    基本模块OK

---
+++ common/src/main/java/com/dlg/fpc/common/TConst.java
@@ -0,0 +1,8 @@
+package com.dlg.fpc.common;
+
+/**
+ * 常量
+ */
+public interface TConst {
+
+}
---
+++ common/src/main/java/com/dlg/fpc/common/util/DateUtils.java
@@ -0,0 +1,8 @@
+package com.dlg.fpc.common.util;
+
+/**
+ * 日期工具
+ */
+public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
+
+}
